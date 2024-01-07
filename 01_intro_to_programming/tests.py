9297720f53952f66e06f14600fe9fab121343a6c
Esteban Cometto <estebancometto@gmail.com>
2024-01-07 19:03:12 -0300

    finish functions chapter

---
+++ 01_intro_to_programming/tests.py
@@ -0,0 +1,35 @@
+# 01_arithmetic_and_variables
+
+# Question 1
+expected_births_per_day = 360000
+
+# 02_functions
+
+# Question 1
+cost_1_bedroom_1_bathroom = 120000
+cost_2_bedroom_1_bathroom = 150000
+
+# Question 2
+expected_options = (
+    170000,
+    190000,
+    200000,
+    210000,
+)
+
+# Question 3
+q3_expected_results = (
+    7.5,
+    10,
+    21.875
+)
+
+# Question 4
+expected_project_cost = 21.6375
+
+# Question 5
+q5_expected_results = ( 
+    15,
+    10,
+    25,
+)
